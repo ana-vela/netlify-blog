87f92cac8c1d92c7e0a0af70b62f0f294e0d56c5
ana-vela <ana@anavela.net>
2021-02-01 10:37:21 -0700

    Add hidden fields to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,55 +1,72 @@
-import React from "react"
-import Helmet from "react-helmet"
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
 
-const ContactPage = ({
-  data: {
-    site
-  },
-}) => {
+const ContactPage = ({ data: { site } }) => {
   return (
     <Layout>
       <Helmet>
         <title>Contact — {site.siteMetadata.title}</title>
-        <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
+        <meta
+          name="description"
+          content={"Contact page of " + site.siteMetadata.description}
+        />
       </Helmet>
       <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
+        <div
+          className="post-thumbnail"
+          style={{
+            backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`,
+            marginBottom: 0,
+          }}
+        >
           <h1 className="post-title">Get in Touch</h1>
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form className="form-container" name="contact" data-netlify-recaptcha="true" data-netlify="true" method="post">
+          <form
+            className="form-container"
+            name="contact"
+            data-netlify-recaptcha="true"
+            data-netlify="true"
+            method="post"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
             <div>
               <label htmlFor="name">Name</label>
-              <input type="text" name="name"/>
+              <input type="text" name="name" />
             </div>
             <div>
               <label htmlFor="email">Email</label>
-              <input type="email" name="email"/>
+              <input type="email" name="email" />
             </div>
             <div>
               <label htmlFor="subject">Subject</label>
-              <input type="text" name="subject"/>
+              <input type="text" name="subject" />
             </div>
             <div>
               <label htmlFor="message">Message</label>
-              <textarea type="text" name="message" ></textarea>
+              <textarea type="text" name="message"></textarea>
             </div>
             <div data-netlify-recaptcha="true"></div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <input
+                type="submit"
+                className="button -primary"
+                style={{ marginRight: 0 }}
+              />
             </div>
           </form>
         </div>
       </div>
     </Layout>
-  )
-}
-export default ContactPage
+  );
+};
+export default ContactPage;
 export const pageQuery = graphql`
-  query ContactPageQuery{
+  query ContactPageQuery {
     site {
       siteMetadata {
         title
@@ -57,4 +74,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
