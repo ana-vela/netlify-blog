846ab3adb1b241939c9aef3e6a9b0c60fd4ac6bd
ana-vela <ana@anavela.net>
2021-02-07 01:48:47 -0700

    Add social media icons to homepage

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -1,5 +1,14 @@
-import React from "react"
-import { StaticQuery, graphql, Link } from "gatsby"
+import React from "react";
+import { StaticQuery, graphql, Link } from "gatsby";
+import {
+  faTwitter,
+  faGithubAlt,
+  faInstagram,
+  faDev,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default () => (
   <StaticQuery
     query={graphql`
@@ -14,15 +23,63 @@ export default () => (
         }
       }
     `}
-    render={data => (
+    render={(data) => (
       <div className="hero-header">
         <div className="headline">{data.site.siteMetadata.home.title}</div>
-        <div 
-          className="primary-content" 
-          dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
+        <div
+          className="primary-content"
+          dangerouslySetInnerHTML={{
+            __html: data.site.siteMetadata.home.description,
+          }}
         />
-        <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
+        <div className="social">
+              <a
+                className="socialIcons"
+                href="https://instagram.com/anaveecodes"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+              <a
+                className="socialIcons"
+                href="https://twitter.com/anaveecodes"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+
+              <a
+                className="socialIcons"
+                href="https://github.com/ana-vela"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faGithubAlt} />
+              </a>
+              <a
+                className="socialIcons"
+                href="https://linkedin.com/in/ana-vela"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+              <a
+                className="socialIcons"
+                href="https://dev.to/anaveecodes"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faDev} />
+              </a>
+            </div>
+
+        <Link to="/contact" className="button -primary">
+          Get in touch &rarr;
+        </Link>
       </div>
     )}
   />
-)
+);
