db92495a889069d1c0a3f38cec11b71f3900b5ad
ana-vela <ana@anavela.net>
2021-02-09 08:10:40 -0700

    Update site favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `Ana Vela | Front End Developer`,
+        short_name: `Ana Vela`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
